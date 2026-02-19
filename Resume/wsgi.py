09c596d34abff4b758715c013ec8a428b5b5a8ce
any <153752169+dineshk042002@users.noreply.github.com>
2026-02-19 23:01:17 +0530

    correct python version

--- Resume/wsgi.py
+++ Resume/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resume.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dinesh.settings')
 
 application = get_wsgi_application()
